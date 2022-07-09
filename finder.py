0bfcc133297379e72ce579217a31ed1b1091f852
Abdallah Eldesouky <52884095+abdallah0x01@users.noreply.github.com>
2022-07-09 03:05:57 +0200

    Update finder.py

--- finder.py
+++ finder.py
@@ -1,9 +1,10 @@
 import os
 to_find = input('Enter file or directory to find: ')
-partation2searchin = input('Enter partation letter to search in: ').upper()
+path = input('Enter path to search in like c:\windows\system32: ')
+
 findings = []
-for dirpath,direnames,filenames in os.walk(f'{partation2searchin}:\\'):
+for dirpath,direnames,filenames in os.walk(fr'{path}'):
     if to_find in filenames or to_find in direnames:
         findings.append(dirpath)
 for finding in findings:
-    print(finding)
+    print(f'what you want in {finding}')
